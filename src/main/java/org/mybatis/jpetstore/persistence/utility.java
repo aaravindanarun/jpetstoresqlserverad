0b754130a395926cac3f7e29953a42714f521c13
aaravindanarun <aravindan.arun@genomels.com>
2017-01-27 17:49:57 +0000

    Update the config.properties filepath for Windows

--- src/main/java/org/mybatis/jpetstore/persistence/utility.java
+++ src/main/java/org/mybatis/jpetstore/persistence/utility.java
@@ -11,14 +11,14 @@ import org.springframework.util.StringUtils;
 public class utility {
 
 	private static final String DELIMITER = "/";
-	private static String CONFIG_LOCATION = "/var/lib/tomcat7/webapps";
+	private static String CONFIG_LOCATION = "d:\home\site\wwwroot\webapps";
 
 	public static Resource getConfigFile(String fileName)
 			throws FileNotFoundException {
 		if (!StringUtils.hasText(fileName)) {
 			throw new FileNotFoundException("File is empty");
 		}
-		System.setProperty(CONFIG_LOCATION, "/var/lib/tomcat7/webapps");
+		System.setProperty(CONFIG_LOCATION, "d:\home\site\wwwroot\webapps");
 	
 		return  new FileSystemResource(
 				System.getProperty(CONFIG_LOCATION) + DELIMITER + fileName);
